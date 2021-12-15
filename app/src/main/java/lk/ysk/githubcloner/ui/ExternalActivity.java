c1f9a62f959d073bb0a299b7b658e4ec7694f41c
Yohan Sandun Kumaranayake <yohan99ysk@gmail.com>
2021-12-15 14:21:22 +0530

    Open app by GitHub links

---
+++ app/src/main/java/lk/ysk/githubcloner/ui/ExternalActivity.java
@@ -0,0 +1,49 @@
+package lk.ysk.githubcloner.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import lk.ysk.githubcloner.R;
+
+public class ExternalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_external);
+        // ATTENTION: This was auto-generated to handle app links.
+
+        Intent appLinkIntent = getIntent();
+        Uri appLinkData = appLinkIntent.getData();
+
+        String[] data = appLinkData.toString().substring(8).split("/");
+        if (data.length >= 3) {
+            if (!data[2].trim().equals("")) {
+                Intent intent = new Intent(ExternalActivity.this, RepoActivity.class);
+                intent.putExtra("url", "https://api.github.com/repos/" + data[1].trim() + "/" + data[2].trim());
+                startActivity(intent);
+                finish();
+                return;
+            } else {
+                Intent intent = new Intent(ExternalActivity.this, UserActivity.class);
+                intent.putExtra("user", data[1].trim());
+                startActivity(intent);
+                finish();
+                return;
+            }
+        } else if (data.length == 2) {
+            Intent intent = new Intent(ExternalActivity.this, UserActivity.class);
+            intent.putExtra("user", data[1].trim());
+            startActivity(intent);
+            finish();
+            return;
+        }
+        Intent intent = new Intent(ExternalActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
